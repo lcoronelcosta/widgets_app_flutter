6586712eee9927517b4e4f7d3e1e5407f05c5104
Luis Coronel <lufecoro@outlook.com>
2024-04-16 19:01:56 -0500

    animated container terminado

--- lib/config/menu/menu_item.dart
+++ lib/config/menu/menu_item.dart
@@ -39,6 +39,12 @@ class MenuItem {
       link: '/snackbars', 
       icon: Icons.info_outline
     ),
+    MenuItem(
+      title: 'Animated container', 
+      subTitle: 'Stateful widget animado', 
+      link: '/animated', 
+      icon: Icons.check_box_outline_blank_rounded
+    ),
   ];
   
 }
